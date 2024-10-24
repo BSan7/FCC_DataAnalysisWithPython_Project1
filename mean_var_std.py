6e7a2032bacb2de5f0d0c79277cfdb6f6987a9bc
BSan7 <santosdelcampobruno@gmail.com>
2024-10-24 14:17:57 +0200

    Add files via upload

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,19 @@
 import numpy as np
 
 def calculate(list):
-
-
-
-
-    return calculations
+    if len(list) != 9:
+        raise ValueError('List must contain nine numbers.')
+    
+    array1 = np.array(list)
+    array1 = array1.reshape(3,3)   #making the 3x3 matrix
+    
+    calculations = {
+        'mean': [array1.mean(axis = 0).tolist(), array1.mean(axis = 1).tolist(), array1.mean()],
+        'variance': [array1.var(axis = 0).tolist(), array1.var(axis = 1).tolist(), array1.var()],
+        'standard deviation': [array1.std(axis = 0).tolist(), array1.std(axis = 1).tolist(), array1.std()],
+        'max': [array1.max(axis = 0).tolist(), array1.max(axis = 1).tolist(), array1.max()],
+        'min': [array1.min(axis = 0).tolist(), array1.min(axis = 1).tolist(), array1.min()],
+        'sum': [array1.sum(axis = 0).tolist(), array1.sum(axis = 1).tolist(), array1.sum()]
+        }
+    
+    return calculations  
